d594a4025c675e94f36fda5f8390864255d50779
igorgaelzer <igor@nordweg.com>
2019-09-28 16:01:46 -0300

    Better datas e prazos

--- app/helpers/shipments_helper.rb
+++ app/helpers/shipments_helper.rb
@@ -1,2 +1,12 @@
 module ShipmentsHelper
+
+  def delivery_icon(due_at = nil, accomplished_at = nil)
+    return nil unless due_at && accomplished_at
+    if accomplished_at.to_date <= due_at.to_date
+      "<i style='font-size: 20px;' class='fas fa-check-circle kt-font-info'></i>".html_safe
+    else
+      "<i style='font-size: 20px;' class='fas fa-exclamation-circle kt-font-warning'></i>".html_safe
+    end
+  end
+
 end
